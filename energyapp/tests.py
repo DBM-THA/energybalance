90f0593653a2768e883a5994894849d02ece1483
TobiasMaile <tobias.maile@hs-augsburg.de>
2025-11-30 18:12:15 +0100

    adding a first test

--- energyapp/tests.py
+++ energyapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Building
+class SimpleTest(TestCase):
 
-# Create your tests here.
+    def test_homepage_status_code(self):
+        """Check that the homepage returns HTTP 200."""
+        response = self.client.get(reverse("dashboard"))
+        self.assertEqual(response.status_code, 200)
