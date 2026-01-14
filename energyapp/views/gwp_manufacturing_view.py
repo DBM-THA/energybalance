9ba6516bc41a6f5c8e4d96ec2803eb475a2022f2
ken020002 <duytr03@Yahoo.com>
2026-01-14 15:49:55 +0100

    Update gwp_manufacturing_view.py

--- energyapp/views/gwp_manufacturing_view.py
+++ energyapp/views/gwp_manufacturing_view.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
 from energyapp.models import Building, GwpManufacturing
 from energyapp.forms import GwpManufacturingForm
 
-@login_required
 def gwp_manufacturing_edit(request, building_id):
     building = get_object_or_404(Building, pk=building_id)
 
