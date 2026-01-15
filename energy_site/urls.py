4f5bb07bf88716b06030ae4e6599195c8353824c
ken020002 <duytr03@Yahoo.com>
2026-01-15 13:27:55 +0100

    Update urls.py

--- energy_site/urls.py
+++ energy_site/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("energyapp.urls")),  # unsere App
+    path("", include("energyapp.urls")),              # unsere App
+    path("accounts/", include("django.contrib.auth.urls")),  # ✅ Login-URLs
 ]
-path("", include("energyapp.urls")),
